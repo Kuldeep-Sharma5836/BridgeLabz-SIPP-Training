44cff10e982c196580b9cfc0043af8cd75abd59a
kuldeepsharma5836 <panditkuldeep2019@gmail.com>
2025-06-13 11:00:15 +0530

    { Kuldeep sharma } level 2 completed

---
+++ Control_Flow/level 2/AllFactorsWhile.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class AllFactorsWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 100;
+        int i = 99;
+        while(i > 0){
+            if(n % i == 0){
+                System.out.println(i + " ");
+            }
+            i--;
+        }
+    }
+}
