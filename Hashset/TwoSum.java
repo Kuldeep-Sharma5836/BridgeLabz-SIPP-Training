cac5d29668404886955fbd0181127ccf6b03a82d
kuldeepsharma5836 <panditkuldeep2019@gmail.com>
2025-07-09 22:55:46 +0530

    Hashset , hashmap assingment completed

--- Hashset/TwoSum.java
+++ Hashset/TwoSum.java
@@ -22,7 +22,10 @@ public class TwoSum {
         System.out.println("Enter size of array:");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter element  ");
+            arr[i] = sc.nextInt();
+        }
 
         System.out.println("Enter target sum:");
         int target = sc.nextInt();
