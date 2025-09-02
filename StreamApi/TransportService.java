342a3326e6e74bf000e8578c0dd6063375a5d5e4
kuldeepsharma5836 <panditkuldeep2019@gmail.com>
2025-09-02 09:35:17 +0530

    Workshop

--- StreamApi/TransportService.java
+++
@@ -1,8 +0,0 @@
-public interface TransportService {
-    String getName();
-    double getFare();
-    void showSchedule();
-    default void printServiceDetails() {
-        System.out.println(getName() + " | Fare: " + getFare());
-    }
-}
